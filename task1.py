5f29fe87d43e1a7bb34cf2ad5cbc13d0fbea56c5
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-10-27 01:04:19 +0200

    feat: implement task1.py

--- task1.py
+++ task1.py
@@ -1,22 +1,10 @@
 from datetime import datetime
-
-
-# функція для розрахунку різниці в днях від заданої дати
-# має повертати ціле число як результат 
 def get_days_from_today(date_str: str) -> int:
-
-    # очікуваний формат дати
     date_format = "%Y-%m-%d"
-
     try:
-        # перетворюємо рядок на об'єкт datetime
         parsed_date = datetime.strptime(date_str, date_format).date()
     except ValueError:
-        # якщо формат невірний, виводимо попередження
         print(f"Неправильний формат дати: '{date_str}'. Використовуйте 'РРРР-ММ-ДД'")
-        return
-
+        return 0 
     today_date = datetime.today().date()
-
-    # повертаємо різницю у днях
     return (today_date - parsed_date).days
