e3677aefe73368ba0417ae1efec49039e746940e
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-10-27 01:04:20 +0200

    feat: implement task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,10 @@
 import random
 
-# генерує відсортований список випадкових чисел у заданому діапазоні
 def get_numbers_ticket(min_val: int, max_val: int, quantity: int) -> list[int]:
-    # перевірка вхідних даних
-    if min_val < 1:
-        return []
-    if max_val > 1000:
-        return []
-    if quantity > (max_val - min_val + 1) or quantity <= 0:
+    is_valid = 1 <= min_val <= max_val <= 1000 and 0 < quantity <= (max_val - min_val + 1)
+    if not is_valid:
         return []
     
-    # використовуємо .sample для вибору унікальних номерів
     result = random.sample(range(min_val, max_val + 1), quantity)
     result.sort()
     
