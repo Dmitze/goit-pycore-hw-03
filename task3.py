4723ec9d61546190be9cf502bcf63e8d34dfc2e6
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-10-27 01:04:20 +0200

    feat: implement task3.py

--- task3.py
+++ task3.py
@@ -1,22 +1,12 @@
 import re
 
 def normalize_phone(phone_number: str) -> str:
-
-    # прибираємо всі символи окрім цифр та '+'
     pattern = r"[^\d\+]"
-    sanitized_number = re.sub(pattern, "", phone_number)
-
-    # тепер перевіряємо, як виглядає номер
-    if sanitized_number.startswith('380'):
-        # якщо він починається з 380, то просто додаємо + попереду
-        return '+' + sanitized_number
-    elif sanitized_number.startswith('0'):
-        # якщо з 0, то додаємо міжнародний код +38
-        return '+38' + sanitized_number
+    p = re.sub(r'\D', '', phone_number)
+    
+    if len(p) == 12 and p.startswith('380'):
+        return '+' + p
+    elif len(p) == 10 and p.startswith('0'):
+        return '+38' + p
     else:
-        # якщо номер вже має +, то нічого не робимо
-        if sanitized_number.startswith('+'):
-            return sanitized_number
-        else:
-            # в іншому випадку, вважаємо, що код країни відсутній
-            return '+38' + sanitized_number
+        return '+38' + p.lstrip('38')
